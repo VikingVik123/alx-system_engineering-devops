723808d5bfbdca303bfde1fa101d0708c1ddca43
VikingVik123 <vodimmegwa@gmail.com>
2024-06-04 16:49:51 -0700

    Fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subscribers of a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    response = requests.get(url, allow_redirects=False)
+    header = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, allow_redirects=False, headers=header)
     if response.status_code == 404:
         return 0
     result = response.json().get("data")
