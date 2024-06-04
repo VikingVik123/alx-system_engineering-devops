e3a034638983bf6fb9a728bf06ca7f7c911b4979
VikingVik123 <vodimmegwa@gmail.com>
2024-06-04 16:41:20 -0700

    Added task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script to return number of subscribers
+"""
+import csv
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the number of subscribers of a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    result = response.json().get("data")
+    return result.get("subscribers")
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+Files for api advanced
