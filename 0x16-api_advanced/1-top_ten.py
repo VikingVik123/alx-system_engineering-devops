d842ce0d638e2745b3dd3a965a5610c90aa65908
VikingVik123 <vodimmegwa@gmail.com>
2024-06-04 17:33:31 -0700

    Tried fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -18,15 +18,18 @@ def top_ten(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False,
-                            params=params)
-    if response.status_code == 404:
-        return None
 
-    result = response.json()
-    if "data" in result and "children" in result["data"]:
-        posts = result["data"]["children"]
-        for post in posts:
-            print(post["data"]["title"])
-    else:
-        print("None")
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        if "data" in data and "children" in data["data"]:
+            posts = data["data"]["children"]
+            for post in posts:
+                print(post["data"]["title"])
+        else:
+            print("No posts found in the subreddit '{}'.".format(subreddit))
+
+    except requests.RequestException as e:
+        print("An error occurred:", e)
